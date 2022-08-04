b758a9fb9b9d5548dcbb16683b9291d69df2cc8b
Yann Martin D'Escrienne <yann.martindes@orange.fr>
2022-08-04 22:34:23 +0200

    get new camera on scene loaded

--- Assets/Scripts/CanvasInst.cs
+++ Assets/Scripts/CanvasInst.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CanvasInst : MonoBehaviour // Use for parent gameobject with singleton childs
 {
+    #region SINGLETON
     private static CanvasInst instance;
     public static CanvasInst Instance { get { return instance; } } // Accessor
 
@@ -17,4 +19,19 @@ public class CanvasInst : MonoBehaviour // Use for parent gameobject with single
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
+    #endregion
+    private Canvas canvas;
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        canvas = GetComponent<Canvas>();
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        canvas.worldCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+
 }
